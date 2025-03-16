491cd68a2ccf77e922ea3b2a314c97a6ac98c775
yanliutafewa <20109301@tafe.wa.edu.au>
2025-03-16 20:28:51 +0800

    wip: Commence Framework
    - Create the db.php config file
    - Create helper.php file with functions

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Database Configuration File
+ *
+ * Filename:        db.php
+ * Location:        config/
+ * Project:         YL-PHP-MVC-Template-Proj
+ * Date Created:    16-03-2025
+ *
+ * Author:          Yan Liu
+ *
+ */
+return [
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'dbname' => 'yl_php_mvc_template_proj',
+    'username' => 'YL_PHP_MVC_TEMPLATE_PROJ_USER',
+    'password' => 'password'
+];
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'yanliutafewa/php-mvc-template',
+        'pretty_version' => '0.1',
+        'version' => '0.1.0.0',
+        'reference' => null,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'yanliutafewa/php-mvc-template' => array(
+            'pretty_version' => '0.1',
+            'version' => '0.1.0.0',
+            'reference' => null,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
